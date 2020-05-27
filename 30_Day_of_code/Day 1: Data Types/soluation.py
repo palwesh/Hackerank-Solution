449daf7826ab0e9c53aee526aec1fb72c1b63fca
Palwesh Sahu <34439580+palwesh@users.noreply.github.com>
2020-05-27 11:02:06 +0530

    Create soluation.py

---
+++ 30_Day_of_code/Day 1: Data Types/soluation.py
@@ -0,0 +1,20 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+
+# Declare second integer, double, and String variables.
+i2 = d2 = s2 = None
+
+# Read and save an integer, double, and String to your variables.
+i2, d2, s2 = int(input()), float(input()), str(input())
+
+# Print the sum of both integer variables on a new line.
+print(i+i2)
+
+# Print the sum of the double variables on a new line.
+print(d+d2)
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s+s2)
